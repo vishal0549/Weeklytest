19a19a9aa45a3da239cd49c53daec7db38aa6a2a
Vishal <43312718+vishal0549@users.noreply.github.com>
2023-03-12 17:28:20 +0530

    Add files via upload

--- 12-march/src/component/Feature.jsx
+++ 12-march/src/component/Feature.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+
+
+const Card = (props) => {
+  return (
+    <div className='featureCard'>
+      <div className='featureCardImg'>
+        <i class={props.icon}></i>
+      </div>
+      <div className='featureCardContent'>
+        <h2>Fully Responsive</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt facere eaque doloremque dicta perspiciatis dolor pariatur sit alias tempora commodi.</p>
+        <a href="#">Read More</a>
+      </div>
+    </div>
+  )
+}
 
 const Feature = () => {
   return (
     <div className='mainFeatureSection'>
         <h1>Our Features</h1>
+        <div className='featureCardContainer'>
+          <Card icon="fa-solid fa-desktop" />
+          <Card icon="fa-solid fa-user" />
+          <Card icon="fa-solid fa-recycle" />
+        </div>
+        
     </div>
   )
 }
