f436def9f338ce01e3dfeb1a64f8950440ebbe27
Vishal <43312718+vishal0549@users.noreply.github.com>
2023-03-12 13:13:59 +0530

    Add files via upload

---
+++ 12-march/src/component/Feature.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div className='mainFeatureSection'>
+        <h1>Our Features</h1>
+    </div>
+  )
+}
+
+export default Feature
---
+++ 12-march/src/component/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from './Content';
+import Feature from './Feature';
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <Content />
+        <Feature />
+    </div>
+  )
+}
+
+export default Main
