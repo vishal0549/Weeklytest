5c1e3d6312ed0cd8196409a21603aef3620231ce
Vishal <43312718+vishal0549@users.noreply.github.com>
2023-04-10 02:15:48 +0530

    Add files via upload

--- 09-April/src/main.jsx
+++ 09-April/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import DataProvider from './components/DataProvider'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render( 
+    <DataProvider>
+      <App />
+    </DataProvider>  
 )
